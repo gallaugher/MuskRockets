dac79bb743c4d0080c718885bcd8c5fe7b4b0b4e
John Gallaugher <john.gallaugher@bc.edu>
2017-11-12 14:15:33 -0500

    Complete, working MuskRockets from class review session

--- MuskRockets/ViewController.swift
+++ MuskRockets/ViewController.swift
@@ -7,19 +7,68 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var rocketImage: UIImageView!
+    @IBOutlet weak var blastOffButton: UIButton!
+    
+    let numberOfRockets = 5
+    var currentRocket = 0
+    var yAtLanding: CGFloat = 0
+    var rocketPlayer = AVAudioPlayer()
+    var offScreen = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        yAtLanding = self.rocketImage.frame.origin.y
+    }
+    
+    func playSound(soundName: String, audioPlayer: inout AVAudioPlayer) {
+        if let sound = NSDataAsset(name: soundName) {
+            do {
+                try audioPlayer = AVAudioPlayer(data: sound.data)
+                audioPlayer.play()
+            } catch {
+                print("ERROR: Data from \(soundName) could not be played")
+            }
+        } else {
+            print("Error reading in sound")
+        }
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func blastOffPressed(_ sender: UIButton) {
+        playSound(soundName: "rocketsound", audioPlayer: &rocketPlayer)
+        // if offScreen == false {
+        if yAtLanding == self.rocketImage.frame.origin.y {
+            UIView.animate(withDuration: 2, animations: {
+                self.rocketImage.frame.origin.y = -(self.rocketImage.frame.height)}) { _ in
+                self.rocketPlayer.stop()}
+            self.blastOffButton.setTitle("Return", for: .normal)
+            offScreen = true
+        } else {
+            UIView.animate(withDuration: 2, animations: {self.rocketImage.frame.origin.y = self.yAtLanding
+            }) { _ in
+                self.rocketPlayer.stop()}
+            self.blastOffButton.setTitle("Blast Off!", for: .normal)
+            offScreen = false
+        }
     }
-
-
+    
+    @IBAction func rocketTapped(_ sender: UITapGestureRecognizer) {
+        
+        currentRocket += 1
+        if currentRocket == numberOfRockets {
+            currentRocket = 0
+        }
+        rocketImage.image = UIImage(named: "rocket\(currentRocket)")
+        
+    }
+    
 }
 
